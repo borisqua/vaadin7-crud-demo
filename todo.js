4d4b5671d5fb0e927062cab3632d892b478430a3
Boris Bronovitsky <boris@bravia.ru>
2019-02-15 00:20:45 +0400

    Initial commit

---
+++ todo.js
@@ -0,0 +1,6 @@
+//1. package template app
+//todo>> 2. acquire HSQLDB
+//todo>> 3. acquire Vaadin 7jk
+//todo>> 2. database and tables creating scripts
+//todo>> 3. Spring boot controller
+//todo>> 4. Spring boot persistence
