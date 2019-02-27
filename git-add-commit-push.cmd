7d105f96b827059738580970975928d311615eb1
Boris Bronovitsky <boris@bravia.ru>
2019-02-27 10:50:54 +0400

    First whole one

--- git-add-commit-push.cmd
+++ git-add-commit-push.cmd
@@ -1,1 +1,9 @@
-git add . && git commit -m "Initial commit jpa+vaadin" && git push origin master
+if [%1]==[/?] goto blank
+
+git add . && git commit -m %1 && git push origin master
+
+goto done
+blank:
+echo add a commit message
+done:
+echo done.
