8359bb751c83cf8e5c4d22ab3f6fe0032228f6cc
Boris Bronovitsky <boris@bravia.ru>
2019-02-28 00:17:43 +0400

    + charts on home page

--- todo.js
+++ todo.js
@@ -19,14 +19,6 @@
 //todo>> 14. email to haulmont
 
 //>>check with Java.util.Date / Java.sql.Date / Java.time mess all around
-//
 // java.util.Date date = java.sql.Date.valueOf(localDate);
 // LocalDate ld = new java.sql.Date(date.getTime()).toLocalDate()
 
-// +79272600466
-// // 79272600466
-// // 7(9272)600-46-64
-// // +7(9272)600-46-64
-// // 8 (846) 278-45-40
-// // 8 846 278 45 40
-// // 8 846 278-45-40
