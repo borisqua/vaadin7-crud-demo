8d46b67dbe784460368c95bfbc4781dda6cb4db4
Boris Bronovitsky <boris@bravia.ru>
2019-02-28 12:39:53 +0400

    ready

--- todo.js
+++ todo.js
@@ -14,9 +14,9 @@
 //>> 10.2 patient form
 //>> 10.3 prescription form
 //>> 11. statistics chart to the Start page
-//todo>> 12. fix error in form validation
-//todo>> 13. publish
-//todo>> 14. email to haulmont
+//>> 12. fix error in form validation
+//todo>> 13. deploy on nginx
+//>> 14. email to haulmont
 
 //>>check with Java.util.Date / Java.sql.Date / Java.time mess all around
 // java.util.Date date = java.sql.Date.valueOf(localDate);
