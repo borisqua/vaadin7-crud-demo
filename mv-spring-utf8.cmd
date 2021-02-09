e531dcdf77aa76d57b138d542a4e8adf147f60c9
Boris Bronovitsky <boris@bravia.ru>
2021-02-09 10:29:57 +0400

    fix update issues

--- mv-spring-utf8.cmd
+++
@@ -1,2 +0,0 @@
-rem chcp 1251
-clear && chcp 65001 && mvn clean spring-boot:run
---
+++ run_in_utf8_codepage.cmd
@@ -0,0 +1,8 @@
+rem to display utf-8 symbols on windows command console correctly
+
+rem windows cirillic codepage
+rem chcp 1251
+
+rem windows utf-8 codepage
+clear && chcp 65001 && mvnw clean spring-boot:run
+
